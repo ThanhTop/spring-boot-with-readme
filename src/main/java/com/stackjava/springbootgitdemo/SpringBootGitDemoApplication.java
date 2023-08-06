41289032dfe8deee00038998ef7050338436cd14
thanhnm <thanhnm@gm-group.vn>
2023-08-06 15:55:10 +0700

    First commit

---
+++ src/main/java/com/stackjava/springbootgitdemo/SpringBootGitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.stackjava.springbootgitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitDemoApplication.class, args);
+	}
+
+}
